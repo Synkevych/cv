8c01050d79f03721670a0ae6e9b1436fa1bd3b48
Synkevych Roman <sunkevu4@gmail.com>
2019-01-31 14:16:52 +0200

    New icon file added.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let cookes = document.getElementById("cookes");
+    function show(){
+        //cookes.classList.add('visible'); 
+        cookes.className = cookes.className.replace('show', '');
+    }
+    setTimeout(show, 2000);
+    // function close(){
+    //     cookes.classList.remove('visible')
+    //     cookes.classList.add('invisible');
+    // }
